b157f866f8c32fd7e9f2ea0f9c4aa7031e819682
lucasRivera21 <lucasriverar2001@gmail.com>
2022-12-13 20:54:12 -0500

    responsive added

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,27 +1,8 @@
-const btnX = document.querySelector('.btn-x');
-const symbolX = document.getElementById('symbolX');
+function cpu(){
+    console.log("INTERFAZ CPU");
+}
 
-const btnO = document.querySelector('.btn-o');
-const symbolO = document.getElementById('symbolO');
-
-const btnAnimate = document.getElementById('btn-color');
-
-btnX.addEventListener('click', (e) =>{
-    btnAnimate.classList.remove('transition-btn-rigth');
-    btnAnimate.classList.add('transition-btn-left');
-
-    console.log(btnAnimate.classList);
-
-    symbolO.style.color = "var(--gray)";
-    symbolX.style.color = "var(--background-black)";
-});
-
-btnO.addEventListener('click', (e) => {
-    btnAnimate.classList.add('transition-btn-right');
-    btnAnimate.classList.remove('transition-btn-left');
-
-    console.log(btnAnimate.classList);
-
-    symbolO.style.color = "var(--background-black)";
-    symbolX.style.color = "var(--gray)";
-})
+function player(){
+    console.log("INTERFAZ PLAYER")
+}
+export {cpu, player};
---
+++ styles/js/start.js
@@ -0,0 +1,39 @@
+import {cpu, player} from './main.js'
+
+const btnX = document.querySelector('.btn-x');
+const symbolX = document.getElementById('symbolX');
+
+const btnO = document.querySelector('.btn-o');
+const symbolO = document.getElementById('symbolO');
+
+const btnAnimate = document.getElementById('btn-color');
+
+var symbol = true;
+
+btnX.addEventListener('click', (e) =>{
+    btnAnimate.classList.remove('transition-btn-right');
+    btnAnimate.classList.add('transition-btn-left');
+
+    symbolO.style.color = "var(--gray)";
+    symbolX.style.color = "var(--background-black)";
+
+    symbol = false;
+});
+
+btnO.addEventListener('click', (e) => {
+    btnAnimate.classList.remove('transition-btn-left')
+    btnAnimate.classList.add('transition-btn-right');
+
+    symbolO.style.color = "var(--background-black)";
+    symbolX.style.color = "var(--gray)";
+
+    symbol = true;
+});
+
+const buttons = document.querySelector('.buttons');
+buttons.addEventListener('click', (e) => {
+    if(e.target && (e.target.tagName === 'P' || e.target.tagName === 'BUTTON')){
+        e.target.classList[0] === 'cpu' ? cpu() : player();
+        localStorage.setItem('selected', symbol);
+    }
+});
