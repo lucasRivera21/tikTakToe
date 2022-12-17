0e47849be95cb7479a96c934bdb9cb07dba34929
lucasRivera21 <lucasriverar2001@gmail.com>
2022-12-16 21:06:02 -0500

    template play add

--- styles/js/main.js
+++ styles/js/main.js
@@ -18,8 +18,9 @@ async function enableCssPlay(){
     document.querySelector('.symbol-and-select').classList.add('transition-up');
     document.querySelector('.vs-cpu').classList.add('transition-right');
     document.querySelector('.vs-player').classList.add('transition-left');
-    await delay(2000);
+    await delay(470);
     document.querySelector('.start').classList.add('enable');
+    document.querySelector('.play').classList.remove('enable');
 }
 
 export {cpu, player};
