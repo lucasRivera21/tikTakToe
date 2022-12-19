1ee7da6798d5c7b0771f4a22fff3d030d7f030eb
lucasRivera21 <lucasriverar2001@gmail.com>
2022-12-18 23:30:37 -0500

    error

--- styles/js/main.js
+++ styles/js/main.js
@@ -1,3 +1,9 @@
+var turn = false;
+var roundCount = 0;
+const vector = [[null, null, null], [null, null, null], [null, null, null]];
+var enableO = false;
+var enableX = false;
+
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
 }
@@ -8,7 +14,8 @@ function cpu(symbol){
 }
 
 function player(){
-    console.log("INTERFAZ PLAYER")
+    console.log(turn);
+    console.log(roundCount);
     enableCssPlay();
     document.querySelector('.player-1').textContent = '(P1)';
     document.querySelector('.player-2').textContent = '(P2)';
@@ -27,71 +34,74 @@ async function enableCssPlay(){
     document.querySelector('.start').classList.add('enable');
     document.querySelector('.play').classList.remove('enable');
 }
-var turn = false;
-var roundCount = 0;
-const vector = [[null, null, null], [null, null, null], [null, null, null]];
+
 function game(){
     const middle = document.querySelector('.middle');
     const turnX = document.querySelector('.turn-x');
     const turnO = document.querySelector('.turn-o');
 
     middle.addEventListener('click', (e) => {
-        if(roundCount < 9){
-            if(turn){
-                turnX.classList.remove('enable');
-                turnO.classList.add('enable');
-            }else{
-                turnX.classList.add('enable');
-                turnO.classList.remove('enable');
+        console.log(e.target);
+        if(e.target.tagName === 'BUTTON'){
+            if(roundCount < 9){
+                if(turn){
+                    turnX.classList.remove('enable');
+                    turnO.classList.add('enable');
+                }else{
+                    turnX.classList.add('enable');
+                    turnO.classList.remove('enable');
+                }
             }
+            roundCount++;
+            switch(e.target.classList[0]){
+                case 'btn-1':
+                    addVector(0,0);
+                    writeXO('1');
+                    break;
+                case 'btn-2':
+                    addVector(0,1);
+                    writeXO('2');
+                    break;
+                case 'btn-3':
+                    addVector(0,2);
+                    writeXO('3');
+                    break;
+                case 'btn-4':
+                    addVector(1,0);
+                    writeXO('4');
+                    break;
+                case 'btn-5':
+                    addVector(1,1);
+                    writeXO('5');
+                    break;
+                case 'btn-6':
+                    addVector(1,2);
+                    writeXO('6');
+                    break;
+                case 'btn-7':
+                    addVector(2,0);
+                    writeXO('7');
+                    break;
+                case 'btn-8':
+                    addVector(2,1);
+                    writeXO('8');
+                    break;
+                case 'btn-9':
+                    addVector(2,2);
+                    writeXO('9');
+                    break;
+                default:
+                    console.log('Unknow');
+            }
+            win();
         }
-        roundCount++;
-        switch(e.target.classList[0]){
-            case 'btn-1':
-                addVector(0,0);
-                writeXO('1');
-                break;
-            case 'btn-2':
-                addVector(0,1);
-                writeXO('2');
-                break;
-            case 'btn-3':
-                addVector(0,2);
-                writeXO('3');
-                break;
-            case 'btn-4':
-                addVector(1,0);
-                writeXO('4');
-                break;
-            case 'btn-5':
-                addVector(1,1);
-                writeXO('5');
-                break;
-            case 'btn-6':
-                addVector(1,2);
-                writeXO('6');
-                break;
-            case 'btn-7':
-                addVector(2,0);
-                writeXO('7');
-                break;
-            case 'btn-8':
-                addVector(2,1);
-                writeXO('8');
-                break;
-            case 'btn-9':
-                addVector(2,2);
-                writeXO('9');
-                break;
-            default:
-                console.log('Unknow');
-        }
-        win();
     })
 }
 
 function writeXO(num){
-    if(document.querySelector('.o-'+num).classList[3] === 'enable' && document.querySelector('.x-'+num).classList[3] === 'enable'){
+    enableO = document.querySelector('.o-'+num).classList.value.includes('enable');
+    enableX = document.querySelector('.o-'+num).classList.value.includes('enable');
+    if(enableX && enableO){
         if(turn){
             document.querySelector('.o-'+num).classList.remove('enable');
             turn = false;
@@ -161,6 +171,18 @@ function win(){
 }
 
 async function winnerAnimation(num1, num2, num3, color, symbol){
+    if (turn){
+        document.querySelector('.x-winner').classList.remove('enable');
+        document.querySelector('.o-winner').classList.add('enable');
+        document.querySelector('.take').style.color = 'var(--blue)';
+        document.querySelector('.name-winner').textContent = 'PLAYER 1 WIN';
+    }else{
+        document.querySelector('.x-winner').classList.add('enable');
+        document.querySelector('.o-winner').classList.remove('enable');
+        document.querySelector('.take').style.color = 'var(--orange)';
+        document.querySelector('.name-winner').textContent = 'PLAYER 2 WIN';
+    }
+    document.querySelector('.dont-click').classList.remove('enable');
     document.querySelector('.'+symbol+'-'+num1).classList.add('winner-animation');
     document.querySelector('.btn-'+num1).style.backgroundColor = `var(--${color})`;
     await delay(200);
@@ -169,7 +191,49 @@ async function winnerAnimation(num1, num2, num3, color, symbol){
     await delay(200);
     document.querySelector('.'+symbol+'-'+num3).classList.add('winner-animation');
     document.querySelector('.btn-'+num3).style.backgroundColor = `var(--${color})`;
-    
+    await delay(1300);
+    document.querySelector('.dont-click').style.backgroundColor = 'var(--background-black)';
+    document.querySelector('.winner-container').classList.remove('enable');
 }
 
+//BTN QUIT
+document.querySelector('.quit').addEventListener('click', () =>{
+    clear();
+    //QUITAR WINNER-ANIMATION
+    document.querySelector('.play').classList.add('enable');
+    document.querySelector('.dont-click').classList.add('enable');
+    document.querySelector('.winner-container').classList.add('enable');
+    document.querySelector('.start').classList.remove('enable');
+    document.querySelector('.symbol-and-select').classList.remove('transition-up');
+    document.querySelector('.vs-cpu').classList.remove('transition-right');
+    document.querySelector('.vs-player').classList.remove('transition-left');
+})
+
+//BTN NEXT
+document.querySelector('.next').addEventListener('click', () =>{
+    console.log('NEXT');
+})
+
+//CLEAR
+const symbols = document.querySelectorAll('.symbol-mid');
+const boxes = document.querySelectorAll('.btn-box');
+function clear(){
+    for(let i=0; i<symbols.length; i++){
+        if(i<3){
+            vector[0][i] = null;
+            vector[1][i] = null;
+            vector[2][i] = null;
+        }
+        symbols[i].classList.add('enable');
+        if(i<9){
+            boxes[i].style.backgroundColor = 'var(--black)';
+        }
+    }
+
+
+
+    turn = false;
+    roundCount = 0;
+    winner = false;
+}
 export {cpu, player};
